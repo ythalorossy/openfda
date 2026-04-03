122787520f1e9b0a1a2e9a5097486573d26d5a52
Ythalo Saldanha <ysaldanha@css.checkbook.org>
2026-04-03 16:31:29 -0400

    feat: add src/drug/index.ts re-exporting all drug tools

---
+++ src/drug/index.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025 Ythalo Saldanha
+ * Licensed under the MIT License
+ */
+
+export { getDrugByName } from './get-drug-by-name.js';
+export { getDrugByGenericName } from './get-drug-by-generic-name.js';
+export { getDrugAdverseEvents } from './get-drug-adverse-events.js';
+export { getDrugsByManufacturer } from './get-drugs-by-manufacturer.js';
+export { getDrugSafetyInfo } from './get-drug-safety-info.js';
+export { getDrugByNdc } from './get-drug-by-ndc.js';
+export { getDrugByProductNdc } from './get-drug-by-product-ndc.js';
